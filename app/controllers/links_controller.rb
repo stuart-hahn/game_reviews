54cf19f34972a17c17bfbd04a22caea64216bf1d
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-18 08:38:20 -0500

    add destroy method to links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -36,6 +36,10 @@ class LinksController < ApplicationController
     end
 
     def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+       
+        redirect_to articles_path
     end
 
     private
